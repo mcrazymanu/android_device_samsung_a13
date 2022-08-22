06751599ec0eea9b35a45ace85f4a56a250bb012
mcrazymanu <mcrazymanu@gmail.com>
2022-08-22 12:02:49 +0530

    main

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a13.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a13-user \
+    twrp_a13-userdebug \
+    twrp_a13-eng
---
+++ twrp_a13.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from a13 device
+$(call inherit-product, device/samsung/a13/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a13
+PRODUCT_NAME := twrp_a13
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A135F
+PRODUCT_MANUFACTURER := samsung
